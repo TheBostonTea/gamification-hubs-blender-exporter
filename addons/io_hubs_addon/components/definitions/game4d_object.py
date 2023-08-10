3b7cedfb0c72ed7260753c29ff0738921031750d
TheBostonTea@TeBunIII <thomas.jong@student.uva.nl>
2023-08-10 14:40:10 +0200

    First steps to importing multiple variables

--- addons/io_hubs_addon/components/definitions/game4d_object.py
+++ addons/io_hubs_addon/components/definitions/game4d_object.py
@@ -1,11 +1,95 @@
-from bpy.props import BoolProperty, EnumProperty, StringProperty
+import bpy
+from bpy.props import BoolProperty, EnumProperty, StringProperty, IntProperty, CollectionProperty
+from bpy.types import PropertyGroup, UIList, Operator, Panel
 from ..hubs_component import HubsComponent
 from ..types import Category, NodeType, PanelType
 from ..game4d_consts import VARIABLE_TYPES
 from ..game4d_utils import game4d_gen_variable_dict
 import json
 
-# id = 0; 
+# id = 0;
+
+class VariableEntry(PropertyGroup):
+    name: StringProperty(
+        name="Variable Name",
+        description="How the variable will be referenced within this object."
+    )
+
+    type: EnumProperty(
+        name="Variable Type",
+        description="The Datatype of the variable.",
+        items=VARIABLE_TYPES,
+        default="string"
+    )
+
+    content: StringProperty(
+        name="Variable Content",
+        description="What the value will be for the variable.",
+    )
+
+
+class VariablesList(UIList):
+    bl_idname = "HUBS_UL_VARIABLES_list"
+
+    # Constants (flags)
+    # Be careful not to shadow FILTER_ITEM (i.e. UIList().bitflag_filter_item)!
+    # E.g. VGROUP_EMPTY = 1 << 0
+
+    # Custom properties, saved with .blend file. E.g.
+    # use_filter_empty: bpy.props.BoolProperty(
+    #     name="Filter Empty", default=False, options=set(),
+    #     description="Whether to filter empty vertex groups",
+    # )
+
+    # Called for each drawn item.
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index, flt_flag):
+
+        # 'DEFAULT' and 'COMPACT' layout types should usually use the same draw code.
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.label(text="{item.name} : {item.type}", icon = icon)
+    
+        # 'GRID' layout type should be as compact as possible (typically a single icon!).
+        elif self.layout_type == 'GRID':
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon = icon)
+
+    # # Called once to draw filtering/reordering options.
+    # def draw_filter(self, context, layout):
+    #     # Nothing much to say here, it's usual UI code...
+    #     pass
+
+    # # Called once to filter/reorder items.
+    # def filter_items(self, context, data, propname):
+    #     # This function gets the collection property (as the usual tuple (data, propname)), and must return two lists:
+    #     # * The first one is for filtering, it must contain 32bit integers were self.bitflag_filter_item marks the
+    #     #   matching item as filtered (i.e. to be shown), and 31 other bits are free for custom needs. Here we use the
+    #     #   first one to mark VGROUP_EMPTY.
+    #     # * The second one is for reordering, it must return a list containing the new indices of the items (which
+    #     #   gives us a mapping org_idx -> new_idx).
+    #     # Please note that the default UI_UL_list defines helper functions for common tasks (see its doc for more info).
+    #     # If you do not make filtering and/or ordering, return empty list(s) (this will be more efficient than
+    #     # returning full lists doing nothing!).
+
+    #     # Default return values.
+    #     flt_flags = []
+    #     flt_neworder = []
+
+    #     # Do filtering/reordering here...
+
+    #     return flt_flags, flt_neworder
+
+class VariablesListNewVariable(Operator):
+
+    bl_idname = "variables.new_item"
+    bl_label = "Add a new item"
+
+    def execute(self, context):
+        # What is the context here?
+        # context.
+
+        return{'FINISHED'}
+
+
 
 class Game4dObject(HubsComponent):
     _definition = {
@@ -29,39 +113,42 @@ class Game4dObject(HubsComponent):
         description="Is this Game4d object active at the start of the runtime? (E.g, does it produce behavior?)",
         default=True
     )
-
-    # Just use an empty string for no variables!
-    # hasVariable: BoolProperty(
-    #     name="Has Variable",
-    #     description="Does this Game4d object hold a variable?",
-    #     default=False
-    # )
   
     # TODO: Deprecate, or at least change. These must not be exported! 
-    variableName: StringProperty(
-        name="Var Name",
-        description="How the internally held variable will be named to be accessible by the game system",
-        default=""
+    # variableName: StringProperty(
+    #     name="Var Name",
+    #     description="How the internally held variable will be named to be accessible by the game system",
+    #     default=""
+    # )
+
+    # variableType: EnumProperty(
+    #     name="Var Type",
+    #     description="How the internally held variable will be interpreted by the game system",
+    #     items=VARIABLE_TYPES,
+    #     default="string"
+    # )
+
+    # variableContent: StringProperty(
+    #     name="Var Content",
+    #     description="What the internally held variable will be for this object",
+    #     default=""
+    # )
+
+    # variables: StringProperty(
+    #     name="Variables",
+    #     description="List of all the variables associated with this object.",
+    #     default="",
+    #     options={'HIDDEN'}
+    # )
+
+    variables: CollectionProperty(
+        type=VariableEntry
     )
 
-    variableType: EnumProperty(
-        name="Var Type",
-        description="How the internally held variable will be interpreted by the game system",
-        items=VARIABLE_TYPES,
-        default="string"
-    )
-
-    variableContent: StringProperty(
-        name="Var Content",
-        description="What the internally held variable will be for this object",
-        default=""
-    )
-
-    variables: StringProperty(
-        name="Vars",
-        description=".json Representation of the variables!",
-        default="",
-        options={'HIDDEN'}
+    active_variable_key: IntProperty(
+        name="Active variable index",
+        description="Active variable index",
+        default=-1
     )
 
     # do I need this?
@@ -76,18 +163,33 @@ class Game4dObject(HubsComponent):
 
     #     return migration_occurred
 
+    def draw(self, context, layout, panel):
+        layout.prop(data=self, property="identifier")
+        layout.prop(data=self, property="isActive")
+
+        layout.label(text='Variables of this Game4d Object:')
+
+        row = layout.row()
+
+        row.template_list(VariablesList.bl_idname, "", self,
+                          "variables", self, "active_variable_key", rows=3)
+
+        # if self.variableName != "":
+        #     layout.prop(data=self, property="variableType")
+        #     layout.prop(data=self, property="variableContent")
+
     def gather(self, export_settings, object):
 
         # Todo: Gather multiple variables from a collection!
-        variables = [game4d_gen_variable_dict(self.variableName, self.variableType, self.variableContent)]
+        # variables = [game4d_gen_variable_dict(self.variableName, self.variableType, self.variableContent)]
 
         varjsonstr = ""
 
     
-        if self.variableName != "":
+        # if self.variableName != "":
 
-            varjsonstr = json.dumps(variables)
-            print(varjsonstr)
+        #     varjsonstr = json.dumps(variables)
+        #     print(varjsonstr)
 
 
         return {
@@ -95,15 +197,23 @@ class Game4dObject(HubsComponent):
             'isActive': self.isActive,
             'variables': varjsonstr
         }
+
+    @ staticmethod
+    def register():
+        bpy.utils.register_class(VariablesList)
+    
+    @ staticmethod
+    def unregister():
+        bpy.utils.unregister_class(VariablesList)
+
+def register_module():
+    bpy.utils.register_class(VariableEntry)
+
+
+def unregister_module():
+    bpy.utils.unregister_class(VariableEntry)
+
     
 
-    def draw(self, context, layout, panel):
-        layout.prop(data=self, property="identifier")
-        layout.prop(data=self, property="isActive")
 
-        layout.prop(data=self, property="variableName")
-
-        if self.variableName != "":
-            layout.prop(data=self, property="variableType")
-            layout.prop(data=self, property="variableContent")
 
